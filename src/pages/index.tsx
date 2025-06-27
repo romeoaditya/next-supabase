36faef117bac3d3f94964828879bd13f321f856c
romeoaditya <romeoaditya22@gmail.com>
2025-06-27 15:34:39 +0700

    feat: ui show menu

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,8 @@
+import {Button} from "@/components/ui/button";
+import {Card, CardContent, CardFooter} from "@/components/ui/card";
 import supabase from "@/lib/db";
 import type {IMenu} from "@/types/menu";
+import Image from "next/image";
 import {useEffect, useState} from "react";
 
 const Home = () => {
@@ -14,8 +17,35 @@ const Home = () => {
     fetchMenus();
   }, [supabase]);
   return (
-    <div>
-      <div>Home</div>
+    <div className="container mx-auto py-8">
+      <h1 className="text-3xl font-bold mb-4">Menu</h1>
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+        {menus.map((menu: IMenu) => (
+          <Card key={menu.id}>
+            <CardContent>
+              <Image
+                src={menu.image}
+                alt={menu.name}
+                width={500}
+                height={500}
+                className="w-full h-[30vh] object-cover rounded-lg"
+              />
+              <div className="mt-4 flex justify-between">
+                <div>
+                  <h4 className="font-semibold text-xl">{menu.name}</h4>
+                  <p>{menu.category}</p>
+                </div>
+                <p className="font-semibold text-2xl">${menu.price}.00</p>
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button className="w-full font-bold cursor-pointer" size={"lg"}>
+                Detail Menu
+              </Button>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
     </div>
   );
 };
